a95202739f99c28a5cdf42fa9072381e4b9af89b
Fernanda Rabaçal <nandarabacal02@hotmail.com>
2022-06-10 14:01:46 -0300

    Inicio a tela Home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,6 +1,7 @@
-import { useNavigate } from "react-router-dom"
 import "./style.css"
 import {Header} from '../../components/Nav'
+import Whatsapp from '../../assets/whatsapp.svg'
+import DogCat from '../../assets/dog-cat-puppy-kitten.png'
 
 export function Home(){
 
@@ -8,6 +9,31 @@ export function Home(){
   return(
     <div className='home-container'>
       <Header/>
+
+      <div className='home-wrapper'>
+      <h1 className="home-title">ANIMAL PET</h1>
+            <h4 className="home-h4">Cuidado completo com o seu animalzinho</h4>
+            <p className="home-content">
+              A Animal Pet possui uma estrutura completa para cuidar do seu pet, desde consultório veterinário, área de banho e tosa, além de acessórios e rações da maior qualidade! Estamos abertos de segunda a sabado, das 8hrs às 18hrs
+            </p>
+            <button className='home-button'>
+            <img src={Whatsapp} alt='whatsapp logo'/>
+             <p>Agende agora Sua tosa</p>
+            </button>
+            <img className="home-img" src={DogCat} alt="two cats and two dogs" />
+            <section className="stats">
+            <div className="stat">
+              <h3>+2.500</h3>
+              <p>Clientes</p>
+            </div><div className="stat">
+              <h3>+2.500</h3>
+              <p>Clientes</p>
+            </div><div className="stat">
+              <h3>+2.500</h3>
+              <p>Clientes</p>
+            </div>
+            </section>
+      </div>
     </div>
   )
 
